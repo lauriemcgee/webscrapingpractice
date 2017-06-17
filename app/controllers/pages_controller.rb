72089691d061ab6fcfc06a10a09f05631f0e7e73
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-06-17 12:01:48 -0500

    index view reflecting the instance methods available to the job title instance

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     document = open('https://www.indeed.com/jobs?q=html&l=Chicago%2C+IL')
     content = document.read
     parsed_content = Nokogiri::HTML(content)
-    @job_titles = parsed_content.css('.jobtitle')
+    @job_titles = parsed_content.css('.jobtitle').first.class.instance_methods.sort
     render 'index.html.erb'
   end
 end
